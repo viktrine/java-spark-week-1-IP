6c206d2b83ed900b3cbce593f5ec08ed162bec47
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-26 01:28:56 +0300

    Create end to end EncodeInput model class

--- src/main/java/tk/itish/models/EncodeInput.java
+++ src/main/java/tk/itish/models/EncodeInput.java
@@ -1,5 +1,34 @@
 package tk.itish.models;
 // define class as model
 public class EncodeInput {
-    // define member
+    // define member variables
+    private String inputString;
+    private int key;
+
+    // define no arg constructor
+    public EncodeInput() {
+    }
+
+    // define argumented constructor
+    public EncodeInput(String inputString, int key) {
+        this.inputString = inputString;
+        this.key = key;
+    }
+
+    // create setters and getters
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
 }
